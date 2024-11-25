a375aebed2c71f7f9a352ea3c410427d9ca7da17
Luanmonteiro <luandasnevesmonteirocardoso@gmail.com>
2024-11-25 20:28:55 -0300

    atualização do código

--- cypress/e2e/pages/LicitacoesPage.cy.js
+++ cypress/e2e/pages/LicitacoesPage.cy.js
@@ -1,32 +1,31 @@
 class LicitacoesPage {
     visitarPagina(url) {
-        cy.visit(url, { timeout: 600000 }); // Timeout ajustado para 60 segundos
+        cy.visit(url, { timeout: 10000 }); // Timeout ajustado para 60 segundos
         cy.url().should("include", "licitacoes");
       }
   
       selecionarAno(ano) {
-        cy.get('p-dropdown#selectYear', { timeout: 10000 }).click();
+        cy.get("p-dropdown#selectYear", { timeout: 10000 }).click();
         cy.get(`li[aria-label="${ano}"]`, { timeout: 10000 }).click();
+      }
+    
+      preencherPalavraChave(chave) {
+        cy.get('li.ui-chips-input-token input[type="text"]', { timeout: 10000 })
+          .type(chave)
+          .type("{enter}");
+      }
+    
+      realizarPesquisa() {
+        cy.get("span.ui-button-text.ui-clickable").contains("Pesquisar").click();
+      }
+    
+      validarValorTotal(valorEsperado) {
+        cy.get("tfoot").within(() => {
+          cy.contains("td", valorEsperado, { timeout: 10000 })
+            .scrollIntoView() // Garante que o elemento está visível na viewport
+            .should("be.visible");
+        });
+      }
     }
-  
-    preencherPalavraChave(chave) {
-      cy.get(
-        'input[placeholder="Pressione ENTER após digitar cada palavra-chave"]',
-        { timeout: 10000 }
-      )
-        .type(chave)
-        .type("{enter}");
-    }
-  
-    realizarPesquisa() {
-      cy.get("span.ui-button-text.ui-clickable").contains("Pesquisar").click();
-    }
-  
-    validarValorTotal(valorEsperado) {
-      cy.get("tfoot").within(() => {
-        cy.contains("td", valorEsperado).should("be.visible");
-      });
-    }
-  }
-  
-  export default new LicitacoesPage();
+    
+    export default new LicitacoesPage();
